477a4e41fb86b59556a2570147663be9e3d5d55b
Jannis Milz <jannis@milz.ch>
2024-01-14 21:57:55 +0100

    WIP setup Kysely

---
+++ db/db.ts
@@ -0,0 +1,23 @@
+import { Database } from './schema.ts' // this is the Database interface we defined earlier
+import { createPool } from 'mysql2' // do not use 'mysql2/promises'!
+import { Kysely, MysqlDialect } from 'kysely'
+
+const dialect = new MysqlDialect({
+  // Currently only placeholders
+  pool: createPool({
+    database: 'test',
+    host: 'localhost',
+    user: 'admin',
+    password: '123',
+    port: 3306,
+    connectionLimit: 10,
+  })
+})
+
+// Database interface is passed to Kysely's constructor, and from now on, Kysely 
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how 
+// to communicate with your database.
+export const db = new Kysely<Database>({
+  dialect,
+})
