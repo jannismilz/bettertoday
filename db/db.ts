e684394ff1fd6868e5430620c8bd0f7029f6b14f
Jannis Milz <jannis@milz.ch>
2024-01-15 20:56:17 +0000

    Run formatter

--- db/db.ts
+++ db/db.ts
@@ -1,27 +1,28 @@
-import { createPool } from 'mysql2' // do not use 'mysql2/promises'!
-import { Kysely, MysqlDialect } from 'kysely'
-import { PersonTable } from './schemas/User.ts'
+import { createPool } from "mysql2"; // do not use 'mysql2/promises'!
+import { Kysely, MysqlDialect } from "kysely";
+import { PersonTable } from "./schemas/User.ts";
 
 interface Database {
-  person: PersonTable
+  person: PersonTable;
 }
 
 const dialect = new MysqlDialect({
   // Currently only placeholders
   pool: createPool({
-    database: process.env.NODE_ENV === "development" ? "development" : "bettertoday",
+    database:
+      process.env.NODE_ENV === "development" ? "development" : "bettertoday",
     host: "database",
     user: process.env.NODE_ENV === "development" ? "root" : process.env.DB_USER,
     password: process.env.DB_PASSWORD || "",
     port: 3306,
     connectionLimit: 10,
-  })
-})
+  }),
+});
 
-// Database interface is passed to Kysely's constructor, and from now on, Kysely 
+// Database interface is passed to Kysely's constructor, and from now on, Kysely
 // knows your database structure.
-// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how 
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
 // to communicate with your database.
 export const db = new Kysely<Database>({
   dialect,
-})
+});
