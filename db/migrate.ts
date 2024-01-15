b481f8d2a7d6aba1b829ede73dfb7b49243b554a
Jannis Milz <jannis@milz.ch>
2024-01-15 20:56:01 +0000

    Adjust migration (since not in production yet) & format

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,7 +1,7 @@
-import { db } from "./db"
-import * as path from 'path'
-import { promises as fs } from 'fs'
-import { FileMigrationProvider, Migrator } from 'kysely'
+import { db } from "./db";
+import * as path from "path";
+import { promises as fs } from "fs";
+import { FileMigrationProvider, Migrator } from "kysely";
 
 const migrator = new Migrator({
   db,
@@ -9,28 +9,28 @@ const migrator = new Migrator({
     fs,
     path,
     // This needs to be an absolute path.
-    migrationFolder: path.join(__dirname, 'db/migrations'),
+    migrationFolder: path.join(__dirname, "migrations"),
   }),
-})
+});
 
 async function migrateToLatest() {
-  const { error, results } = await migrator.migrateToLatest()
+  const { error, results } = await migrator.migrateToLatest();
 
   results?.forEach((it) => {
-    if (it.status === 'Success') {
-      console.log(`migration "${it.migrationName}" was executed successfully`)
-    } else if (it.status === 'Error') {
-      console.error(`failed to execute migration "${it.migrationName}"`)
+    if (it.status === "Success") {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === "Error") {
+      console.error(`failed to execute migration "${it.migrationName}"`);
     }
-  })
+  });
 
   if (error) {
-    console.error('failed to migrate')
-    console.error(error)
-    process.exit(1)
+    console.error("failed to migrate");
+    console.error(error);
+    process.exit(1);
   }
 
-  await db.destroy()
+  await db.destroy();
 }
 
-migrateToLatest()
+migrateToLatest();
--- db/migrations/2024_01_15_12_57_20_initialize.ts
+++ db/migrations/2024_01_15_12_57_20_initialize.ts
@@ -5,120 +5,202 @@ export async function up(db: Kysely<any>): Promise<void> {
     .createTable("users")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("firstname", "varchar", (col) => col.notNull())
-    .addColumn("lastname", "varchar", (col) => col.notNull())
-    .addColumn("email", "varchar", (col) => col.unique().notNull())
-    .addColumn("avatar_url", "varchar")
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("first_name", "varchar(255)", (col) => col.notNull())
+    .addColumn("last_name", "varchar(255)", (col) => col.notNull())
+    .addColumn("email", "varchar(255)", (col) => col.unique().notNull())
+    .addColumn("avatar_url", "varchar(255)")
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("projects")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("user_id", "integer", (col) => col.references("users.id").notNull())
-    .addColumn("name", "varchar", (col) => col.notNull())
-    .addColumn("description", "varchar")
-    .addColumn("cover_url", "varchar")
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("user_id", "integer", (col) =>
+      col.references("users.id").notNull(),
+    )
+    .addColumn("name", "varchar(255)", (col) => col.notNull())
+    .addColumn("description", "varchar(255)")
+    .addColumn("cover_url", "varchar(255)")
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("project_todos")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("project_id", "integer", (col) => col.references("projects.id").notNull())
-    .addColumn("todoist_project_id", "varchar", (col) => col.notNull())
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("project_id", "integer", (col) =>
+      col.references("projects.id").notNull(),
+    )
+    .addColumn("todoist_project_id", "varchar(255)", (col) => col.notNull())
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("project_counters")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("project_id", "integer", (col) => col.references("projects.id").notNull())
+    .addColumn("project_id", "integer", (col) =>
+      col.references("projects.id").notNull(),
+    )
     .addColumn("goal", "float4", (col) => col.notNull())
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("project_habits")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("project_id", "integer", (col) => col.references("projects.id").notNull())
+    .addColumn("project_id", "integer", (col) =>
+      col.references("projects.id").notNull(),
+    )
     .addColumn("weekdays", "json", (col) => col.notNull())
     .addColumn("frequency", "integer", (col) => col.notNull())
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("project_deadlines")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("project_id", "integer", (col) => col.references("projects.id").notNull())
+    .addColumn("project_id", "integer", (col) =>
+      col.references("projects.id").notNull(),
+    )
     .addColumn("end", "timestamp", (col) => col.notNull())
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("project_stopwatches")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("project_id", "integer", (col) => col.references("projects.id").notNull())
+    .addColumn("project_id", "integer", (col) =>
+      col.references("projects.id").notNull(),
+    )
     .addColumn("start", "timestamp", (col) => col.notNull())
     .addColumn("stop", "timestamp")
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("project_stopwatche_resets")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("project_stopwatch_id", "integer", (col) => col.references("project_stopwatches.id").notNull())
+    .addColumn("project_stopwatch_id", "integer", (col) =>
+      col.references("project_stopwatches.id").notNull(),
+    )
     .addColumn("reset", "timestamp")
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("shared_dashboards")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("user_id", "integer", (col) => col.references("users.id").notNull())
-    .addColumn("name", "varchar", (col) => col.notNull())
-    .addColumn("password", "varchar")
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("user_id", "integer", (col) =>
+      col.references("users.id").notNull(),
+    )
+    .addColumn("name", "varchar(255)", (col) => col.notNull())
+    .addColumn("password", "varchar(255)")
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 
   await db.schema
     .createTable("shared_dashboard_projects")
     .ifNotExists()
     .addColumn("id", "integer", (col) => col.primaryKey().autoIncrement())
-    .addColumn("shared_dashboard_id", "integer", (col) => col.references("shared_dashboards.id").notNull())
-    .addColumn("project_id", "integer", (col) => col.references("projects.id").notNull())
-    .addColumn("next_project", "integer", (col) => col.references("projects.id"))
-    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`now()`).notNull())
-    .addColumn("updated_at", "timestamp", (col) => col.defaultTo(sql`now()`).onUpdate("set default").notNull())
+    .addColumn("shared_dashboard_id", "integer", (col) =>
+      col.references("shared_dashboards.id").notNull(),
+    )
+    .addColumn("project_id", "integer", (col) =>
+      col.references("projects.id").notNull(),
+    )
+    .addColumn("next_project", "integer", (col) =>
+      col.references("projects.id"),
+    )
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .addColumn("updated_at", "timestamp", (col) =>
+      col
+        .defaultTo(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`)
+        .notNull(),
+    )
     .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable("users").execute()
-  await db.schema.dropTable("projects").execute()
-  await db.schema.dropTable("project_todos").execute()
-  await db.schema.dropTable("project_counters").execute()
-  await db.schema.dropTable("project_habits").execute()
-  await db.schema.dropTable("project_deadlines").execute()
-  await db.schema.dropTable("project_stopwatches").execute()
-  await db.schema.dropTable("project_stopwatche_resets").execute()
-  await db.schema.dropTable("shared_dashboards").execute()
-  await db.schema.dropTable("shared_dashboard_projects").execute()
+  await db.schema.dropTable("users").execute();
+  await db.schema.dropTable("projects").execute();
+  await db.schema.dropTable("project_todos").execute();
+  await db.schema.dropTable("project_counters").execute();
+  await db.schema.dropTable("project_habits").execute();
+  await db.schema.dropTable("project_deadlines").execute();
+  await db.schema.dropTable("project_stopwatches").execute();
+  await db.schema.dropTable("project_stopwatche_resets").execute();
+  await db.schema.dropTable("shared_dashboards").execute();
+  await db.schema.dropTable("shared_dashboard_projects").execute();
 }
