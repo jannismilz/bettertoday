57d445ec32bbcb161ef86d6145ea89a7a087d899
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-01-16 11:22:45 +0000

    Rename shared dashboards table migration

---
+++ db/migrations/2024_01_16_11_25_07_rename_shared_dashboards.ts
@@ -0,0 +1,35 @@
+import { sql, Kysely } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable("shared_dashboards")
+    .renameTo("shared_views")
+    .execute()
+
+  await db.schema
+    .alterTable("shared_dashboard_projects")
+    .renameTo("shared_view_projects")
+    .execute()
+
+  await db.schema
+    .alterTable("shared_view_projects")
+    .renameColumn("shared_dashboard_id", "shared_view_id")
+    .execute()
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable("shared_view_projects")
+    .renameColumn("shared_view_id", "shared_dashboard_id")
+    .execute()
+
+  await db.schema
+    .alterTable("shared_view_projects")
+    .renameTo("shared_dashboard_projects")
+    .execute()
+
+  await db.schema
+    .alterTable("shared_views")
+    .renameTo("shared_dashboards")
+    .execute()
+}
