06b4bd2dc1e4077d9f9cd4be3a19922843aba279
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-01-16 15:03:40 +0000

    Remov debugging stuff

--- db/repositories/UserRepository.ts
+++ db/repositories/UserRepository.ts
@@ -2,15 +2,9 @@ import { db } from "../db";
 import { NewUser } from "../schemas/Users";
 
 export async function upsertUser(user: NewUser) {
-  const userExists = await db.selectFrom("users").selectAll().where("email", "=", user.email).executeTakeFirst()
-  console.log(userExists);
+  const userExists = await db.selectFrom("users").where("email", "=", user.email).executeTakeFirst()
 
+  if (userExists) return await db.updateTable("users").set(user).where("email", "=", user.email).executeTakeFirst()
 
-  if (userExists) {
-    const res = await db.updateTable("users").set(user).where("email", "=", user.email).executeTakeFirst()
-    console.log(res);
-  }
-
-  const res = await db.insertInto("users").values(user).executeTakeFirst()
-  console.log(res);
+  return await db.insertInto("users").values(user).executeTakeFirst()
 }
