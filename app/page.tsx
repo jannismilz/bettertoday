cc6c8e52aaae7d5dbb620d57c3d8ed95a7476241
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-01-16 15:54:51 +0000

    Add test data

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello {user?.name}
+      <h1>Hey {JSON.stringify(user)}</h1>
     </main>
   );
 }
