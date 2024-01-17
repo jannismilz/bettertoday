28667a44fec8e50b4f231d593e93f888c06ae5f7
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-01-17 14:29:17 +0000

    Add hero icons and adjust create project button

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
 import { Separator } from "@/components/ui/separator";
 import { authOptions } from "@/lib/auth";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { getServerSession } from "next-auth";
 import ProjectForm from "@/components/projectForm";
 import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 export default async function Home() {
   const authSession = await getServerSession(authOptions);
@@ -30,9 +15,13 @@ export default async function Home() {
       <Separator className="my-4" />
       <div className="flex flex-wrap justify-center gap-4">
         <ProjectForm>
-          <Button variant="outline">Open Drawer</Button>
+          <Button variant="outline" className="flex w-full gap-1">
+            <PlusIcon className="h-4 w-4" />
+            Create a Project
+          </Button>
         </ProjectForm>
       </div>
+      <Separator className="my-4" />
     </main>
   );
 }
