070bed3d0656c7ec114adf267f8c9d8e8afc65cd
Jannis Milz <jannis@milz.ch>
2024-01-19 13:14:27 +0000

    Implement PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  manifest: "/manifest.json",
   title: "Better Today",
   description:
     "Your personal progress tracker with a nice UI and simple functionality. Share your progress as well!",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  fallbacks: {
+    document: "/offline",
+  },
+});
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
